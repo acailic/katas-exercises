3016a7634b9d0fb691a9dcaa5a39150076307ff6
aleksandarilic <a.ilic@vegait.rs>
2021-09-02 01:02:23 +0200

    update ContextAbstractions.update Advanced

--- scala-katas/src/main/scala/com/rockthejvm/Advanced.scala
+++ scala-katas/src/main/scala/com/rockthejvm/Advanced.scala
@@ -1,5 +1,9 @@
 package com.rockthejvm
 
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Advanced extends App{
 
   // lazy evaluation
@@ -11,6 +15,47 @@ object Advanced extends App{
   }
   val eagerValue= aLazyValueWithSideEffect +1
 
-  def methodWichCanReturnNull(): String = "Helloo oooo"
+  def methodWhichCanReturnNull(): String = "Helloo oooo"
+  val anOption= Option(methodWhichCanReturnNull()) //
+  // option = "collections" which contains one element, or None
+  // map, flatMap, filter
+
+
+  def methodWhichCanTrowException(): String = throw new RuntimeException
+  val aTry = Try(methodWhichCanTrowException())
+  //
+  val anotherStringProcessing = aTry match {
+    case Success(validValue) => s"Its $validValue success."
+    case Failure(ex) => s"Here is the failure: $ex."
+  }
+  println(aTry)
+  println(anotherStringProcessing)
+  val aFuture = Future {
+    println("Loading......")
+    Thread.sleep(2000)
+    println("Loading 2......")
+    87
+  }
+
+  // future is a "collection" which contains a value when it's evaluated
+  // future is composable with map, flatMap and filter
+
+  /**
+  * Implicits basics
+  */
+  // #1: implicit arguments
+  def aMethodWithImplicitArgs(implicit arg: Int) = arg + 1
+  implicit val myImplicitInt = 46
+  println(aMethodWithImplicitArgs)  // aMethodWithImplicitArgs(myImplicitInt)
+
+  // #2: implicit conversions
+  implicit class MyRichInteger(n: Int) {
+    def isEven(): Boolean = n % 2 == 0
+  }
+
+  println("MyRichInteger")
+  println(23.isEven()) // new MyRichInteger(23).isEven()
+  // use this carefully
+
 
 }
---
+++ scala-katas/src/main/scala/com/rockthejvm/ContextAbstractions.scala
@@ -0,0 +1,86 @@
+package com.rockthejvm
+
+import com.rockthejvm.PatternMatching.Person
+
+object ContextAbstractions extends App {
+  /*
+  scala 3
+   */
+
+  /*
+  Context parameters/arguments
+   */
+  val aList = List(2, 1, 3, 4, 5)
+  val anOrderedList = aList.sorted // ordering
+  // contextual argument.
+
+  given descendingOrder: Ordering[Int]
+  = Ordering.fromLessThan(_ > _) // a,b => a>b
+
+  // analogus to implicit val
+  // in scala 3 , alows to chose should we have context arguments
+
+  trait Combinator[A] {
+    def combine(x: A, y: A): A
+  }
+
+  def combineAll[A](list: List[A])(combinator: Combinator[A]): A = {
+    list.reduce((a, b) => combinator.combine(a, b))
+  }
+
+  val theSum = combineAll(aList)
+
+  // combineAll(List(1,2,3,4)
+
+  /*
+  Given places
+  - local scope
+  - imported scope
+  - companion scope
+      - companion of List
+      - companion of int
+   */
+
+  // context bounds
+  def combineAll_v2[A](list: List[A])(using
+
+  Combinator[A]
+  ):
+  A = ???
+
+  def combineAll_v3[A: Combinator](list: List[A]): A = ???
+  /*
+   context params are used:
+   - type classes
+   - dependency objections
+   - context dependent
+   - type level programing
+   */
+
+  /*
+   2.  - extension methods
+   */
+
+  case clase Person (name: String) {
+    def greet(): String = s"Hi, my name is $name"
+  }
+
+  extension(string: String)
+     def great(): String = new Person().greet()
+
+
+  val danielsGreeting = "Daniel".greeting() // " type enrichment" or pimping
+
+    // power, context + extensions
+  extension [A] (list: List[A])
+    def combineAllValues(using combinator: Combinator[A]): A =
+      list.reduce(combinator.combine)
+
+  val theSum_2 = aList.combineAllValues
+
+  def main(args: Array[String]): Unit = {
+    println(anOrderedList)
+    println(theSum_2)
+
+  }
+}
